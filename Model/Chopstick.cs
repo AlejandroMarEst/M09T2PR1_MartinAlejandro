770c57321c6d7cfd7e5241bb637b2cc85fc1d571
AlejandroMarEst <alejandro.martin.7e8@itb.cat>
2025-04-29 12:52:09 +0200

    Created classes for commensals and chopsticks. Closes #1

---
+++ Model/Chopstick.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M09T2PR1_MartinAlejandro.Model
+{
+    public class Chopstick
+    {
+        public bool BeingUsed { get; set; }
+    }
+}
---
+++ Model/Commensal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M09T2PR1_MartinAlejandro.Model
+{
+    public class Commensal
+    {
+        public int NumCommensal { get; set; }
+        public int MaxSecondsWithoutEating { get; set; }
+        public int TimesAte { get; set; }
+    }
+}
